7f88ba69ac780f50fe6258ff53c42fbb8ef7447e
bgyuri76 <bgyuridev@gmail.com>
2016-07-26 15:15:45 +0300

    working app

---
+++ RetrofitApp/app/src/main/java/com/example/gyuri/retrofitapp/rest/ApiInterface.java
@@ -0,0 +1,19 @@
+package com.example.gyuri.retrofitapp.rest;
+
+/**
+ * Created by gyuri on 7/26/2016.
+ */
+import com.example.gyuri.retrofitapp.model.MoviesResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+
+public interface ApiInterface {
+    @GET("movie/top_rated")
+    Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/{id}")
+    Call<MoviesResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
+}
